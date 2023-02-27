e048f3ce00671db1991cb8da41fd3ce413d64b9c
Felicia Burtscher <felicia.burtscher@uni.lu>
2023-02-27 22:55:12 +0100

    Add script performing pvalue analysis for gender and age group
    In this commit, we add a script that performs kw and wilcoxon tests
    on data, differentiating by genes and grouping by:
    
    - Gender (to no avail so far)
    - Age group (25 year groups)
    - Combined diagnosis + age group
    
    In this script we also have some box plot creations.
    
    This script is very much a WIP

---
+++ biomarker_selection/additional-pvalue-calculations/age_sex_pvalue_calculations.R
@@ -0,0 +1,157 @@
+library(here)
+library(dplyr)
+
+source(here("functions", "data_processing_functions.R"))
+source(here("functions", "auxiliary_statistical_functions.R"))
+
+datasets_root_directory <- define_datasets_root()
+
+### Read the table
+kth <- readxl::read_xlsx(file.path(datasets_root_directory,
+                                   "KTH/KTH AD dataset for MIRIADE biomarker selection sample info and results.xlsx"))
+
+### Change the names into HGNC symbols (prefixes)
+colnames(kth) <- sapply(colnames(kth), take_only_pre_underscore_substring)
+
+### Melt the dataframe, so there's only one readout variable
+melted_kth <-
+  reshape2::melt(kth, id = 1:9,
+                 variable.name = "HGNC_Symbol", value.name = "median_ab_readout") %>%
+  #dplyr::filter(Diagnosis == "AD") %>%
+  convert_age_column_to_age_group_column(sym("Age"), sym("Age_Group")) %>%
+  dplyr::select(Diagnosis, Age_Group, Gender, HGNC_Symbol, median_ab_readout)
+
+
+### Run Kruskal-Wallis test for all biomarkers grouped by gender
+kw_per_hgnc_gender_grouped <-
+  sapply(
+    unique(melted_kth$HGNC_Symbol),
+    kruskal_wallis_test_for_sample_groups,
+    melted_kth,
+    sym("HGNC_Symbol"),
+    sym("Gender"),
+    sym("median_ab_readout"))
+
+significant_kw_per_hgnc_gender_grouped <-
+  keep_only_significant_entries(
+    melted_kth,
+    differentiating_feature_symbol = sym("HGNC_Symbol"),
+    kw_per_differentiating_feature = kw_per_hgnc_gender_grouped,
+    significance_limit = 0.1)
+
+### Age_Group
+kw_per_hgnc_age_group_grouped <-
+  sapply(
+    unique(melted_kth$HGNC_Symbol),
+    kruskal_wallis_test_for_sample_groups,
+    melted_kth,
+    sym("HGNC_Symbol"),
+    sym("Age_Group"),
+    sym("median_ab_readout"))
+
+significant_kw_per_hgnc_age_group_grouped <-
+  keep_only_significant_entries(
+    melted_kth,
+    differentiating_feature_symbol = sym("HGNC_Symbol"),
+    kw_per_differentiating_feature = kw_per_hgnc_age_group_grouped,
+    significance_limit = 0.05)
+
+significant_kw_wilcox_age_group_grouped <-
+  test_pairwise_wilcoxon(
+    melted_kth,
+    sym("HGNC_Symbol"),
+    significant_kw_per_hgnc_age_group_grouped,
+    sym("Age_Group"),
+    sym("median_ab_readout"))
+
+kth_summary <- melted_kth %>%
+dplyr::summarise(mean = mean(median_ab_readout),
+                 sd = sd(median_ab_readout),
+                 median = median(median_ab_readout),
+                 .by = c(Diagnosis, Age_Group, Gender, HGNC_Symbol))
+
+boxplot(EDN1 ~ Diagnosis, data = kth)
+
+################################################################################
+# EXPERIMENTAL
+################################################################################
+
+supercharged_melt <-
+  melted_kth %>%
+  dplyr::filter(Diagnosis == "AD" | Diagnosis == "Control") %>%
+  dplyr::mutate(Diagnosis_Age_Group = paste(Diagnosis, Age_Group)) %>%
+  dplyr::select(Diagnosis_Age_Group, Gender, HGNC_Symbol, median_ab_readout)
+
+kw_per_hgnc_diagnosis_age_group <-
+  sapply(
+    unique(supercharged_melt$HGNC_Symbol),
+    kruskal_wallis_test_for_sample_groups,
+    supercharged_melt,
+    sym("HGNC_Symbol"),
+    sym("Diagnosis_Age_Group"),
+    sym("median_ab_readout"))
+
+signif_kw_per_hgnc_diagnosis_age_group <-
+  keep_only_significant_entries(
+    supercharged_melt,
+    differentiating_feature_symbol = sym("HGNC_Symbol"),
+    kw_per_differentiating_feature = kw_per_hgnc_diagnosis_age_group,
+    significance_limit = 0.05)
+
+signif_kw_wilcox_diagnosis_age_group <-
+  test_pairwise_wilcoxon(
+    supercharged_melt,
+    sym("HGNC_Symbol"),
+    signif_kw_per_hgnc_diagnosis_age_group,
+    sym("Diagnosis_Age_Group"),
+    sym("median_ab_readout"))
+
+mutated_kth <- kth %>%
+  convert_age_column_to_age_group_column(sym("Age"), sym("Age_Group")) %>%
+  dplyr::filter(Diagnosis == "AD" | Diagnosis == "Control") %>%
+  dplyr::mutate(Diagnosis_Age_Group = paste(Diagnosis, Age_Group))
+
+create_box_plot <- function(dataframe, x_column, y_column) {
+  x_column_symbol <- sym(x_column)
+  y_column_symbol <- sym(y_column)
+  return(
+    ggplot(dataframe, aes(x = !!x_column_symbol, y = !!y_column_symbol)) +
+      geom_boxplot() +
+      scale_x_discrete() #+
+      #labs(y = y_column)
+  )
+}
+
+create_box_plots_of_all_columns_starting_at_column_number <-
+  function(dataframe, x_column, y_starting_column) {
+  number_of_columns <- length(names(dataframe))
+  y_column_names <- names(dataframe)[y_starting_column:number_of_columns]
+  new_length <- number_of_columns - y_starting_column
+  box_plots <- vector("list", length = new_length)
+  for (i in 1:new_length) { # TODO: Possibly replace with sapply
+    box_plots[[i]] <- create_box_plot(dataframe, x_column, y_column_names[[i]])
+  }
+  return(box_plots)
+}
+create_box_plot(mutated_kth, "Diagnosis_Age_Group", "BDP1")
+boxplot(BDP1 ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(CHGB ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(DDAH1 ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(EDN1 ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(IL1B ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(MDH1 ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(PEBP1 ~ Diagnosis_Age_Group, data = mutated_kth)
+
+boxplot(S100B ~ Diagnosis_Age_Group, data = mutated_kth)
+
+################################################################################
+#####################################EMIF#######################################
+################################################################################
+emif <- read_password_xlsx(file.path(datasets_root_directory,
+                                   "EMIF-AD MBD Study/Clinical_EMIFMBD_MIRIADE.xlsx"), 1)
