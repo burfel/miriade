107094bf54de076314e672f6a0789b689df299ff
Felicia Burtscher <felicia.burtscher@uni.lu>
2023-03-02 22:59:14 +0100

    [pruning-networks]: Extract get_all_interactions_that_have_backing_uniprots
    In this commit, we extract the process of using the interactions excel
    and the dictionary excel to have a df that has both network objects
    and uniprots of target and source.
    In this funtction, empty or invalid uniprot rows are filtered out, and
    there is a bool controlling whether you end up with uniprots or
    network object names.

--- biomarker_selection/networks/network_expansion.R
+++ biomarker_selection/networks/network_expansion.R
@@ -3,41 +3,13 @@ library(here)
 source(here("functions", "biomarker-network_functions.R"))
 source(here("functions", "data_processing_functions.R"))
 from_metacore_path <- here("biomarker_selection", "networks", "from-metacore")
-miriade_genes <- readxl::read_xls(here(from_metacore_path, 
-                                        "all-MIRIADE-biomarker-candidates_analyse-networks_genes.xls"), 
-                                        skip = 2) %>%
-  #dplyr::select(`SwissProt IDs`, `Network Object Name`) %>%
-  dplyr::select(`Input IDs`, `Network Object Name`) %>%
-  # Remove useless "No ID" rows
-  #dplyr::filter(`SwissProt IDs`!="No ID") %>%
-  dplyr::filter(`Input IDs`!="No ID") %>%
-  dplyr::distinct(`Network Object Name`, .keep_all = TRUE)
 
-miriade_interactions <- readxl::read_xls(here(from_metacore_path, 
-                                        "all-MIRIADE-biomarker-candidates_analyse-networks_interactions.xls"), 
-                                        skip = 2) %>%
-  dplyr::select(`Network Object \"FROM\"`, `Network Object \"TO\"`) %>%
-  dplyr::distinct() # 5 NAs
-
-miriade_uniprot_interactions <- translate_interactions_using_dictionary(
-        interactions_df = miriade_interactions,
-        interaction_columns = c("Network Object \"FROM\"", "Network Object \"TO\""),
-        dictionary_df = miriade_genes,
-        #dictionary_columns = c("Network Object Name", "SwissProt IDs"),
-        dictionary_columns = c("Network Object Name", "Input IDs"),
-        new_interaction_names = c("UniProt_FROM", "UniProt_TO"),
-        only_interaction_columns = F
-        ) %>%
-  dplyr::filter(!grepl(';', UniProt_FROM)) %>%
-  dplyr::filter(!grepl(';', UniProt_TO))
-# Produce a list of all UniProts in the file
-# uniprots_miriade <- extract_all_distinct_objects(miriade_uniprot_interactions,
-                                     # c("UniProt_TO", "UniProt_FROM"))
-
-# After cleaning with the uniprots, let's continue with the network objects
-miriade_interactions <- miriade_uniprot_interactions %>%
-  dplyr::select(`Network Object \"FROM\"`, `Network Object \"TO\"`) %>%
-  dplyr::distinct()
+miriade_interactions <- get_all_interactions_that_have_backing_uniprots(
+  dataset_directory = from_metacore_path,
+  interactions_file = "all-MIRIADE-biomarker-candidates_analyse-networks_interactions.xls",
+  dictionary_file = "all-MIRIADE-biomarker-candidates_analyse-networks_genes.xls",
+  return_as_uniprots = T
+)
 
 # Produce a list of all network objects
 network_objects_miriade <- extract_all_distinct_objects(miriade_interactions,
--- functions/biomarker-network_functions.r
+++ functions/biomarker-network_functions.r
@@ -175,3 +175,70 @@ unify_graphs <- function(graph_list, vertices_by_type, extended_source_list,
     color_edges_based_on_sources(extended_source_list, color_sequence)
   return(unified_graph)
 }
+
+###############################################################################
+#' Getting all network object interactions that have valid uniprots for the
+#' network objects
+#'
+#' @param dataset_directory The directory where the interactions dataset and
+#'        and the dictionary (network object to uniprot) dataset sit
+#' @param interactions_file The file where the interactions between the
+#'        network objects are
+#' @param dictionary_file The file translating network objects to uniprots
+#' @param interaction_columns The column names of the source and target of
+#'        the interactions
+#' @param dictionary_columns The column names of the network object to uniprot
+#'        translation
+#' @param uniprot_interaction_names The name to give to the uniprot based
+#'        interaction columns
+#' @param return_as_uniprots whether to return the interaction df with uniprots
+#'
+#' @return A df that contains the interactions either as target and source
+#'         network objects or target and source uniprots.
+#'         The rows are unique.
+###############################################################################
+get_all_interactions_that_have_backing_uniprots <- function(
+    dataset_directory,
+    interactions_file,
+    dictionary_file,
+    interaction_columns = c("Network Object \"FROM\"", "Network Object \"TO\""),
+    dictionary_columns = c("Network Object Name", "Input IDs"),
+    uniprot_interaction_names = c("UniProt_FROM", "UniProt_TO"),
+    return_as_uniprots = FALSE)
+{
+  require(dplyr)
+  require(here)
+  dictionary <- readxl::read_xls(here(dataset_directory, dictionary_file), skip = 2) %>%
+    dplyr::select(all_of(dictionary_columns)) %>%
+    # Remove useless "No ID" rows
+    dplyr::filter(!!sym(dictionary_columns[[2]])!="No ID") %>%
+    dplyr::distinct(!!sym(dictionary_columns[[1]]), .keep_all = TRUE) %>%
+    dplyr::filter(!grepl(';', !!sym(dictionary_columns[[2]])))
+
+  interactions <- readxl::read_xls(here(from_metacore_path, interactions_file),
+                                   skip = 2) %>%
+    dplyr::select(all_of(interaction_columns)) %>%
+    dplyr::distinct()
+
+  uniprot_interactions <- translate_interactions_using_dictionary(
+    interactions_df = interactions,
+    interaction_columns = interaction_columns,
+    dictionary_df = dictionary,
+    dictionary_columns = dictionary_columns,
+    new_interaction_names = uniprot_interaction_names,
+    only_interaction_columns = F
+  )
+
+  # After cleaning with the uniprots, let's continue with the network objects
+  # or uniprot, depending on return_as_uniprots
+  if (return_as_uniprots) {
+    returning_columns <- uniprot_interaction_names
+  }
+  else {
+    returning_columns <- interaction_columns
+  }
+
+  return(uniprot_interactions %>%
+    dplyr::select(all_of(returning_columns)) %>%
+    dplyr::distinct())
+}
