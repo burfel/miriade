1d112eef1dcfc9a12155a5ccb6e50c7bc055e381
Felicia Burtscher <felicia.burtscher@uni.lu>
2023-03-06 23:11:48 +0100

    [Correlation]: Separate the heatmap plotting to a function

--- biomarker_selection/EDA/correlation.R
+++ biomarker_selection/EDA/correlation.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggplot2)
 
 source(here("functions", "data_processing_functions.R"))
-source(here("functions", "auxiliary_statistical_functions.R"))
+source(here("functions", "heatmap_functions.R"))
 
 datasets_root_directory <- define_datasets_root()
 
@@ -19,10 +19,7 @@ olink <- read_df_from_file(file.path(datasets_root_directory,
 olink_corr <- corrr::correlate(olink[,4:ncol(olink)]) %>%
   stretch()
 
-olink_corr %>%
-  ggplot(aes(x, y, fill = r)) +
-  geom_tile() +
-  scale_fill_gradient(low = "blue", high = "red")
+plot_correlation_heatmap(olink_corr, "Olink")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ####                                KTH                                     ####
@@ -35,14 +32,4 @@ colnames(kth) <- sapply(colnames(kth), take_only_pre_underscore_substring)
 
 kth_corr <- corrr::correlate(kth) %>% stretch()
 
-kth_corr %>%
-  ggplot(aes(x, y, fill = r)) +
-  geom_tile()  +
-  labs(x = NULL, y = NULL, fill = "Pearson's\nCorrelation", title="Correlations in KTH") +
-  # map a red, white and blue color scale to correspond to -1:1 sequential gradient
-  scale_fill_gradient2(mid="#FBFEF9",low="#0C6291",high="#A63446", limits=c(-1,1)) +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  # remove excess space on x and y axes
-  scale_x_discrete(expand=c(0,0)) +
-  scale_y_discrete(expand=c(0,0))
+plot_correlation_heatmap(kth_corr, "KTH")
