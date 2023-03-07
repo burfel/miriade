a0209703decb825b1b62f35104d0047c55569f0b
Felicia Burtscher <felicia.burtscher@uni.lu>
2023-03-07 22:15:26 +0100

    [BiomarkerDistribution]: Add mapping function from hgnc to uniprot

--- functions/mapping_functions.R
+++ functions/mapping_functions.R
@@ -27,3 +27,40 @@ map_uniprot_to_hgnc_and_cbind <- function(df_with_uniprot_column, new_column_nam
   
   return(cbind(hgnc, df_with_uniprot_column))
 }
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#' Map uniprot to hgnc symbol and add it as column to the df
+#'
+#' @param df_with_hgnc_column
+#' @param new_column_name
+#'
+#' @return The same `df_with_hgnc_column` with an added column for uniprot
+#'         given the name 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+map_hgnc_to_uniprot_and_cbind <- function(df_with_hgnc_column, new_column_name)
+{
+  require('biomaRt')
+  if(!exists("ensembl_h")) {
+    # Assign to global environment for caching purposes
+    ensembl_h <<- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+  }
+  
+  id_mapping <- get_hgnc_to_uniprot_mapping(df_with_hgnc_column)
+  
+  uniprot <- named_list(new_column_name, sapply(
+    df_with_hgnc_column$Name,
+    function(x) with(id_mapping, uniprotswissprot[hgnc_symbol == x][1])))
+  
+  return(cbind(uniprot, df_with_hgnc_column))
+}
+
+# Auxiliary function to map_hgnc_to_uniprot_and_cbind
+get_hgnc_to_uniprot_mapping <- function(df_with_hgnc_column)
+{
+  id_mapping <- getBM(attributes = c("hgnc_symbol", "uniprotswissprot"),
+                      filters = "hgnc_symbol",
+                      values = df_with_hgnc_column,
+                      mart = ensembl_h)
+  
+  id_mapping <- dplyr::filter(id_mapping, uniprotswissprot != "")
+}
