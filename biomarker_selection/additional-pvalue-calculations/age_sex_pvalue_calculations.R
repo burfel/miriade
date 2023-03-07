e20c90c1646de772129e3058bfe9334371377032
Felicia Burtscher <felicia.burtscher@uni.lu>
2023-03-07 21:12:41 +0100

    [BiomarkerDistribution]: Add auxiliary file to meld all fractured datasets
    In this commit we add a file to perform the melding of the raw emif and
    olink datasets, and we source this file in the `age_sex_pvalue_calculations`

--- biomarker_selection/additional-pvalue-calculations/age_sex_pvalue_calculations.R
+++ biomarker_selection/additional-pvalue-calculations/age_sex_pvalue_calculations.R
@@ -1,10 +1,10 @@
 library(here)
 library(dplyr)
 
-source(here("functions", "data_processing_functions.R"))
+# This loads the melded emif and olink dataframes to be used later in the file
+# It also calls to set the dataset root directory
+source(here("biomarker_selection", "EDA", "meld_all_fractured_datasets.R"))
 source(here("functions", "auxiliary_statistical_functions.R"))
-
-datasets_root_directory <- define_datasets_root()
 
 ### Read the table
 kth <- readxl::read_xlsx(file.path(datasets_root_directory,
@@ -79,54 +79,8 @@ mutated_kth <- kth %>%
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ####                                EMIF                                    ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-if(!file.exists(file.path(datasets_root_directory,
-                          "EMIF-AD MBD Study/dcast_proteins.tsv"))) {
-  melted_proteins <-
-    read_df_from_file(file.path(datasets_root_directory,
-                                "EMIF-AD MBD Study/data_protein.tsv"))
-  casted_proteins <-
-    reshape2::dcast(melted_proteins,
-            Assay.ID ~ PEPTIDE.SEQUENCE,
-            value.var = "VALUE") %>%
-    dplyr::select_if(~!any(is.na(.)))
-  
-  write.table(casted_proteins, file = file.path(datasets_root_directory, "EMIF-AD MBD Study/dcast_proteins.tsv"),
-              sep = "\t", row.names = F)
-}
 
-emif_dir <- "EMIF-AD MBD Study"
-
-excludes <- list(
-  c("Fu","Study","Diag_num", "VisitTime", "Studyname", "Center", "Eduy",
-    "Eduy_IMP", "BL_Diaggroups", "AMYLOIDstatus", "ABmethod",
-    "MMSE", "DateMMSE", "DateBloodCollection", "DateCSFCollection", "DateMRI",
-    "DatePET","FU_available", "LastFU", "LastFU_Date", "LastFU_Diagnosis",
-    "MCI_Convert", "CTR_Convert", "Plasma_ID", "MRI_ID", "CSF_ID", "Local_AB42",
-    "Local_AB42_Abnormal", "Local_AB42_Cutoff", "Local_PTAU", "Local_PTAU_Abnormal",
-    "Local_PTAU_Cutoff", "Local_TTAU", "Local_TTAU_Abnormal", "Local_TTAU_Cutoff",
-    "AmyPET_SUVR", "AmyPET_SUVRAbnormal", "AmyPET_SUVRCutoff", "AmyPET_Method",
-    "AmyPET_Tracer", "AmyPET_Manufacturer", "AB_Zscore", "Ptau_ASSAY_Zscore",
-    "Ttau_ASSAY_Zscore", "Central_CSF_AB40", "Central_CSF_AB42",
-    "Central_CSF_AB4240ratio", "Central_CSF_ratiodich", "Hippo_sum_FS_adj",
-    "LOCAL_CSFAssay", "LOCAL_CSFAssayCompany", "CDR_Global", "IntervalAB_Cog",
-    "IntervalAB_MRI", "IntervalAB_Blood", "IntervalMRI_Blood", "Sel_intervalABCog",
-    "Sel_intervalABMRI", "Sel_intervalABBlood", "Sel_intervalCogMRI",
-    "Sel_intervalCogBlood", "Sel_intervalMRIBlood", "APOEdich", "APOEdich_IMP",
-    "APOE_Allele1", "APOE_Allele2", "APOE_flag"),
-  c("Sample.Type", "Time.Point", "Tissue.Type", "Platform.ID", "Sample.Code"),
-  c())
-
-emif <-
-  meld_fractured_dataset(
-    dataset_paths = c(
-      file.path(datasets_root_directory, emif_dir,"Clinical_EMIFMBD_MIRIADE.xlsx"),
-      file.path(datasets_root_directory, emif_dir,"samples.tsv"),
-      file.path(datasets_root_directory, emif_dir,"dcast_proteins.tsv")),
-    by_columns = setNames(c("Subject.ID","Assay.ID"), c("SubjectId","Assay.ID")),
-    exclude_columns = excludes) %>%
-  dplyr::mutate(Gender = case_when(Gender == 0 ~ 'm', Gender == 1 ~ 'f', TRUE ~ as.character(Gender)))
-
-### Melt the dataframe, so there's only one readout variable
+### Melt the emif dataframe, so there's only one readout variable
 melted_emif <-
   reshape2::melt(emif, id = 1:5,
                  variable.name = "UniProt", value.name = "Value") %>%
@@ -158,23 +112,8 @@ emif_age_group_grouped_pvals <- perform_kw_wilcoxon_according_to_grouping(
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ####                                 OLINK                                  ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-olink_dir <- "Olink/OLINK basic data files"
 
-excludes <- list(
-  c("X","Olink_project", "Olink_plate"),
-  c("sam_ind", "X")
-)
-
-olink <-
-  meld_fractured_dataset(
-    dataset_paths = c(
-      file.path(datasets_root_directory, olink_dir,"clin_bl_basic_04022020.csv"),
-      file.path(datasets_root_directory, olink_dir,"protein_data_LODmax_10percent_outliers_rm_missing_imputed_16112020.tsv")),
-    by_columns = setNames(c("SampleId"), c("SampleId")),
-    exclude_columns = excludes)  %>%
-  dplyr::mutate(sex = case_when(sex == 1 ~ 'm', sex == 2 ~ 'f', TRUE ~ sex))
-
-### Melt the dataframe, so there's only one readout variable
+### Melt the olink dataframe, so there's only one readout variable
 melted_olink <-
   reshape2::melt(olink, id = 1:4,
                  variable.name = "Gene_name", value.name = "Value") %>%
